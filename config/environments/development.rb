e62faca20b7ff0192f2ab895fdccbe5f8b052071
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-03-27 05:59:45 +0000

    devise実装 新規登録及びログイン

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "8d22fbbe558043308c08e492728b31bd.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
