df08c090c655ca3f5d470fac8c0c370c0bc67574
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-04-16 13:12:46 +0000

    新規投稿画面レイアウト完成

--- app/models/content.rb
+++ app/models/content.rb
@@ -6,9 +6,17 @@ class Content < ApplicationRecord
 
   validates :order, numericality: { only_integer: true }
   validates :hour, presence: true
-  validates :rate, numericality: {
-    less_than_or_equal_to: 5,
-    greater_than_or_equal_to: 1}, presence: true
+  validates :place, presence: true
+  validates :explanation, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :telephonenumber, presence: true
+  validates :access, presence: true
+  validates :businesshours, presence: true
+  validates :price, presence: true
+  validates :stay_time, presence: true
+  validates :reservation, presence: true
+  validates :rate, presence: true
 
   def get_image(width, height)
     unless image.attached?
